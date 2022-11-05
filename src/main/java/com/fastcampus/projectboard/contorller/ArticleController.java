392bb2f97bc8e2e7bf61c4727617f467b9df7942
sGOM <pooh6195@naver.com>
2022-11-05 16:43:40 +0900

    #1 - 게시글 controller 위치 변경
    실수로 test에 생성된 게시글 controller를 main으로 이동

--- src/main/java/com/fastcampus/projectboard/contorller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/contorller/ArticleController.java
@@ -1,9 +1,16 @@
 package contorller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles() {
+        return "articles/index";
+    }
+
 }
