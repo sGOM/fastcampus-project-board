463f4cdc10666ddc030565db3d2f239b91fd6702
sGOM <pooh6195@naver.com>
2022-11-05 18:05:04 +0900

    #1 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/main/java/com/fastcampus/projectboard/contorller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/contorller/ArticleController.java
@@ -3,6 +3,7 @@ package com.fastcampus.projectboard.contorller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,7 +16,16 @@ public class ArticleController {
     public String articles(ModelMap map) {
         // data 넘기기
         map.addAttribute("articles", List.of());
+
         return "articles/index";
     }
 
+    @GetMapping("/{articleId}")
+    public String articles(@PathVariable Long articleId, ModelMap map) {
+        // TODO: 구현할 때 여기에 실제 데이터를 넘겨줘야함
+        map.addAttribute("article", "article");
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
+    }
 }
