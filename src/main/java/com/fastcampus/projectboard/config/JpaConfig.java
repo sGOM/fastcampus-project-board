d2bb8f9c6f855bd1f8ce4c1440e4923079f39fd7
sGOM <pooh6195@naver.com>
2022-11-26 21:28:51 +0900

    #46 - 시큐리티 설정과 자바 대응 코드 구현
    스프링 부트 2.7 과 기이드에 맞춰서
    시큐리티를 설정하고, 인증 정보를 db로부터 부르는 빈과 패스워드 인코더 빈까지 등록
    영향 받는 부분을 서비스 코드와 컨트롤러에 반영

--- src/main/java/com/fastcampus/projectboard/config/JpaConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/JpaConfig.java
@@ -1,9 +1,13 @@
 package com.fastcampus.projectboard.config;
 
+import com.fastcampus.projectboard.dto.security.BoardPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -13,6 +17,13 @@ public class JpaConfig {
 
     @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.of("gom"); // TODO: 스프링 스큐리티로 인증 기능을 붙이게 될 때, 수정하기
+        // Security Context를 가져옴
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                // Authentication 정보를 가져옴
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(BoardPrincipal.class::cast)
+                .map(BoardPrincipal::getUsername);
     }   // 누가 로그인을 했을지를 통해 현재 사용자로 auditing
 }
--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -1,8 +1,19 @@
 package com.fastcampus.projectboard.config;
 
+import com.fastcampus.projectboard.domain.UserAccount;
+import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.dto.security.BoardPrincipal;
+import com.fastcampus.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,8 +22,43 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        // mvcMatcher = antMatcher + Spring Pattern Matching 추가
+                        .mvcMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "/articles",
+                                "/articles/search-hashtag"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .formLogin().and()
+                .logout()
+                        .logoutSuccessUrl("/")
+                        .and()
                 .build();
+    }
+
+    // 이와 같이 작성하면 보안 서비스를 받을 수 없음, 위에 Filter 에 추가되는 것이 권장됨
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        // Spring Security 검사에서 제외할 부분들
+//        // static resource, css, js 등등
+//        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username:  " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
